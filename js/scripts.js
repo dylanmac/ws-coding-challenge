d92fb35cbcb3d5039c0e8620162ee404361545bd
Dylan MacDonald <dylanmac@gmail.com>
2018-02-23 13:28:57 -0800

    Continued progress

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ ws.ecom = ws.ecom || {};
 
 ws.ecom.product = {
 
-
     getJSONP: function(url) {
 
         var script = window.document.createElement('script');
@@ -18,86 +17,41 @@ ws.ecom.product = {
 
     },
 
-    createElementWithClass: function(elem, cls) {
-        var element = document.createElement(elem);
-        element.setAttribute('class', cls);
-        return element;
-    },
-
-    // iterator: function(obj, path) {
-    //     var parts = path.split('.'),
-    //         rv,
-    //         count = 0;
-    //
-    //     obj.forEach(function(elem) {
-    //         //console.log(elem[parts[0]]);
-    //         if (elem.parts !== 'undefined' || elem.parts !== null) {
-    //             rv = elem[parts[count]];
-    //         }
-    //         ++this.count;
-    //     })
-    //     return rv;
-    //
-    //     // parts = path.split(".");
-    //     // obj.forEach(function(elem) {
-    //     //     console.log(elem[parts[0]]);
-    //     //     if (parts.length == 1) {
-    //     //         return elem[parts[0]];
-    //     //     }
-    //     //     //return iterator(elem[parts[0]], parts.slice(1).join("."));
-    //     // })
-    // },
-
-    iterator: function(path) {
-        var path = eval(path);
-        obj.forEach(function(elem) {
-            if (elem.path !== 'undefined' || elem.path !== null) {
-                return elem.path;
-            }
-        })
-        // parts = path.split(".");
-        // obj.forEach(function(elem) {
-        //     console.log(elem[parts[0]]);
-        //     if (parts.length == 1) {
-        //         return elem[parts[0]];
-        //     }
-        //     //return iterator(elem[parts[0]], parts.slice(1).join("."));
-        // })
+    removeElementsByClass: function(parentElem, className) {
+        var elements = parentElem.getElementsByClassName(className);
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
     },
 
     callback: function(data) {
-        var productRow = document.querySelector(".container .row"),
+        var productRow = document.querySelector(".container .row .main-col .row"),
+            productComponent = productRow.querySelector(".category-grid"),
             products = data.groups;
 
         for (i = 0; i < products.length; i++) {
-            var catGrid = this.createElementWithClass('div', 'col-md-4 category-grid'),
-                catComp = this.createElementWithClass('div', 'category-component'),
-                productImg = this.createElementWithClass('div', 'category-image'),
-                quicklink = this.createElementWithClass('a', 'quicklink'),
-                productDesc = this.createElementWithClass('p', 'desc'),
-                pricing = this.createElementWithClass('ul', 'pricing'),
-                low = this.createElementWithClass('li', 'low'),
-                high = this.createElementWithClass('li', 'high');
-            if (products[i].priceRange.selling.high !== 'undefined' || products[i].priceRange.selling.high !== null) {
-                high.innerHTML = products[i].priceRange.selling.high;
-            }
-            // low.appendChild(pricing);
-            // high.appendChild(pricing);
-            pricing.appendChild(low);
-            pricing.appendChild(high);
-            catComp.appendChild(pricing);
-            catGrid.appendChild(catComp);
-            productRow.appendChild(catGrid);
-        }
-        // productRow.appendChild(catGrid);
-        // productRow.appendChild(catGrid);
-        // productRow.appendChild(catGrid);
-        // productRow.appendChild(catGrid);
+            var newComponent = productComponent.cloneNode(true);
 
-        // news.appendChild(h5);
-        // var p = document.createElement("p");
-        // p.innerHTML = products[i].author;
-        // news.appendChild(p);
+            newComponent.querySelector(".category-image").style.backgroundImage = "url(" + products[i].images[0] + ")";
+            newComponent.querySelector(".quicklook").setAttribute('href', '#' + products[i].id);
+            newComponent.querySelector(".desc").innerText = products[i].name;
+            if (products[i].priceRange) {
+                if (products[i].priceRange.selling) {
+                    if (products[i].priceRange.selling.high) {
+                        newComponent.querySelector(".high").innerText = '$' + products[i].priceRange.selling.high;
+                    }
+                    if (products[i].priceRange.selling.low) {
+                        newComponent.querySelector(".low").innerText = '$' + products[i].priceRange.selling.low + ' - ';
+                    }
+                }
+            } else {
+                this.removeElementsByClass(newComponent, 'low');
+                newComponent.querySelector(".high").innerText = '$' + products[i].price.selling;
+            }
+            productRow.appendChild(newComponent);
+
+        }
+        productRow.removeChild(productRow.querySelector(".category-grid"));
 
     },
 
